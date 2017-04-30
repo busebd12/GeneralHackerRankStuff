c574ca6a17bee6897298551ee709e40d449fcb2c
Brendan Busey <brendanbusey@Brendans-MBP.home>
2017-04-30 12:44:31 -0400

    Adding solution for 'Stdin And Stdout I'

---
+++ Java/JavaStdinAndStdoutI/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class solution
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner=new Scanner(System.in);
+
+		int first=scanner.nextInt();
+
+		int second=scanner.nextInt();
+
+		int third=scanner.nextInt();
+
+		scanner.close();
+
+		System.out.println(first);
+
+		System.out.println(second);
+
+		System.out.println(third);
+	}
+}
+
