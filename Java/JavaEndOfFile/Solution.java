9c3e08a3e6f4a8dc09aaf0a0ade36fa593a68e64
Brendan Busey <brendanbusey@Brendans-MBP.home>
2017-05-11 20:17:26 -0400

    Adding solution for 'Java End-of-file' challenge

---
+++ Java/JavaEndOfFile/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        
+        int lineNumber=1;
+        
+        while(scanner.hasNext())
+        {
+            String line=scanner.nextLine();
+            
+            System.out.println(lineNumber + " " + line);
+            
+            ++lineNumber;
+        }
+    }
+}
