6186a083f734eda313ab294a0eb721eb2af48b9c
Brendan Busey <brendanbusey@Brendans-MacBook-Pro.local>
2017-05-18 20:34:03 -0400

    Adding solution to 'Java Substring'

---
+++ Java/JavaSubstring/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        
+        String S=scanner.next();
+        
+        int start=scanner.nextInt();
+        
+        int end=scanner.nextInt();
+        
+        System.out.println(S.substring(start, end));
+    }
+}
