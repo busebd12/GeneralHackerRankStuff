6183ef151cb90d75c83fbfa678c3ff13b84a7217
Brendan Busey <brendan.busey@alumni.wfu.edu>
2018-01-07 19:03:34 -0500

    Adding restructured Linux folder

---
+++ LinuxShell/Bash/TheWorldOfNumbers/TheWorldOfNumbers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read firstNumber
+
+read secondNumber
+
+echo "$((firstNumber+secondNumber))";
+
+echo "$((firstNumber-secondNumber))";
+
+echo "$((firstNumber*secondNumber))";
+
+echo "$((firstNumber/secondNumber))";
